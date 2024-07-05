37820589f8504ba658fe0a46412a0afe26f928dd
X-DX <aruproy5637@gmail.com>
2024-07-05 22:01:42 +0530

    current window print

---
+++ Basics/currentWindowContents.js
@@ -0,0 +1,5 @@
+//Write a JavaScript program to print the current window contents.
+function printCurrentPage(){
+    // call the window.print() method to initiate the printing of the current page
+    window.print();
+}
