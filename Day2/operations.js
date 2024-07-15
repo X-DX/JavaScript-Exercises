8c550de96367ff85689de650daab1db4c6710580
X-DX <aruproy5637@gmail.com>
2024-07-15 08:09:30 +0530

    day 2

---
+++ Day2/operations.js
@@ -0,0 +1,62 @@
+// Arithmatic Operations
+// Task 1
+console.log("Add two number",9+2);
+
+// Task 2
+console.log("Subtract two number",9-2);
+
+// Task 3
+console.log("Multiple two number",9*2);
+
+// Task 4
+console.log("Division two number",20 / 2);
+
+// Task 5
+console.log("Reminder of two number",19 % 2);
+
+// Assignment Operators
+// Task 6
+let a = 10;
+a += a;
+console.log("Task 6", a);
+
+// Task 7
+let b = 10;
+b -= b;
+console.log("Task 7", b);
+
+// Comparision Operators
+// Task 8
+let x = 100;
+let y = 200;
+if(x > y){
+    console.log("X Grater:", x);
+}else if(x < y){
+    console.log("Y Grater:", y);
+}
+
+// Task 9
+let a2 = 10;
+let b2 = 5;
+let c2 = 10;
+
+console.log(a2 >= b2);  
+console.log(a2 >= c2);  
+console.log(b2 >= a2);  
+
+
+let x2 = 3;
+let y2 = 7;
+let z2 = 3;
+
+console.log(x2 <= y2); 
+console.log(x2 <= z2);  
+console.log(y2 <= x2);  
+
+// Task 10
+let aa = "Arup"
+console.log("==",aa == "Arup");
+
+let bb = 10;
+let cc = "10";
+console.log("===",bb === cc);
