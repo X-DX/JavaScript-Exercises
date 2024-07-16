2425abdb3b6ef3f2f91cc634c81d3cbd1091fd76
X-DX <aruproy5637@gmail.com>
2024-07-16 20:52:10 +0530

    day 3

--- Day3/control.js
+++ Day3/control.js
@@ -70,3 +70,101 @@ let number33 = 15;
 // Log the results
 console.log(`Numbers are: ${number11}, ${number22}, ${number33}`);
 console.log(findLargest(number11, number22, number33));
+
+
+// Task 4
+let today = new Date().getDay();
+// console.log(today);
+switch(today){
+    case 0:
+        console.log("Sunday");
+        break;
+
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("Tuesday");
+        break;
+
+    case 3:
+        console.log("wednesday");
+        break;
+    
+    case 4:
+        console.log("Thurday");
+        break;
+
+    case 5:
+        console.log("Friday");
+        break;
+
+    case 6:
+        console.log("Satarday");
+        break;
+
+    default:
+        console.log("No Day");
+}
+
+// Task 5
+function getGrade(score) {
+    let grade;
+
+    switch (true) {
+        case (score >= 90):
+            grade = 'A';
+            break;
+        case (score >= 80):
+            grade = 'B';
+            break;
+        case (score >= 70):
+            grade = 'C';
+            break;
+        case (score >= 60):
+            grade = 'D';
+            break;
+        case (score >= 50):
+            grade = 'E';
+            break;
+        default:
+            grade = 'F';
+            break;
+    }
+
+    return grade;
+}
+
+// Example usage:
+let score = 85;
+let grade = getGrade(score);
+console.log(`The grade for a score of ${score} is ${grade}.`); // Outputs: The grade for a score of 85 is B.
+
+
+// Task 6
+function checkOddEven(number) {
+    return (number % 2 === 0) ? 'Even' : 'Odd';
+}
+
+// Example usage:
+let number = 5;
+console.log(`The number ${number} is ${checkOddEven(number)}.`); // Outputs: The number 5 is Odd.
+
+
+// Task 7
+function isLeepYear(year){
+    if((year % 4 && year & 100 !== 0) || (year % 400 === 0)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let year = new Date().getFullYear();
+// console.log(year);
+if(isLeepYear(year)){
+    console.log(`${year} is a leep year`);
+}else{
+    console.log(`${year} is not a not yaer`);
+}
