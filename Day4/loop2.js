6a6554430b0387be5e9d9ede970b70a23535c4e2
X-DX <aruproy5637@gmail.com>
2024-07-17 08:32:52 +0530

    loop 2

---
+++ Day4/loop2.js
@@ -0,0 +1,13 @@
+// print no 1 - 10 using for loop and while loop
+function loopFunc()
+{
+    for(let i = 1; i<=10; i++){
+        console.log("for loop",i);
+    }
+
+    let j =10;
+    while(j <= 10)
+    {
+        console.log("While Loop", j);
+    }
+}
