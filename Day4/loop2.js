f2e59b0c01504dba7e210babb949867ec2357847
X-DX <aruproy5637@gmail.com>
2024-07-17 16:05:33 +0530

    loop day 4

--- Day4/loop2.js
+++ Day4/loop2.js
@@ -5,9 +5,34 @@ function loopFunc()
         console.log("for loop",i);
     }
 
-    let j =10;
+    let j =1;
     while(j <= 10)
     {
         console.log("While Loop", j);
+        j++;
     }
 }
+
+// loopFunc();
+
+// multiplication table script
+function mulFuc(num){
+    for(let i=1; i<= 10;i++){
+        console.log(i*num);
+    }
+}
+// mulFuc(5);
+
+// Pattern Printing Script
+function printPattern(rows){
+    for(let i =1; i<= rows; i++){
+        let pattern = '';
+        for(let j=1;j<=i; j++){
+            pattern += "*";
+        }
+        console.log(pattern);
+    }
+}
+// printPattern(5);
+
+// 
